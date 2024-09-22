6cf647b5ed73beeeb00a259d74c894c28a38d897
Luigi Morel <morelmiles@gmail.com>
2024-09-22 17:26:00 +0300

    feat: add lexer, tokenizer and REPL

---
+++ todos.md
@@ -0,0 +1,3 @@
+# attach line number and filename to the lexer [page 10]
+
+# the reader to fully support Unicode (and emojis!)
---
+++ token/token.go
@@ -0,0 +1,66 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENTIFIER = "IDENTIFIER"
+	INT        = "INT"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	// DELIMITERS
+	SEMICOLON = ";"
+	COMMA     = ","
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(identifier string) TokenType {
+	if tok, ok := keywords[identifier]; ok {
+		return tok
+	}
+
+	return IDENTIFIER
+}
